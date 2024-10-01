619c3586c840fe61c8db748981f7c32c4ae8092a
rommel alvarado <devegus@gmail.com>
2024-10-01 15:58:04 -0500

    updated h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export const Hero = (props) => {
       <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
         <div className="w-full max-w-xl mx-auto flex-1">
           <h1 className="mb-6 text-4xl font-bold sm:text-5xl" data-sb-field-path="heading">
-            {props.heading}
+            Buy Sara's delicious Cookies!
           </h1>
           {props.body && (
             <Markdown options={{ forceBlock: true }} className="mb-6 text-lg" data-sb-field-path="body">
