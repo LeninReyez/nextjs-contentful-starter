572c08c03eb0654513f18854a62dc98ebfb07cc3
Rommel <devegus@gmail.com>
2024-10-25 18:18:54 -0500

    updated hero intro

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,21 @@ const themeClassMap = {
   imgLeft: 'md:flex-row-reverse',
   imgRight: 'md:flex-row',
 };
-const heading = "Oak & Pine Bakehouse"
-const content = "Craving a sweet treat without the guilt? You’ve come to the right place! At Oak & Pine, we believe that healthy eating shouldn’t mean sacrificing flavor. Our delicious cookies are crafted with wholesome ingredients, bursting with natural goodness, and designed to satisfy your cravings.From rich, nutty flavors to delightful fruity blends, our cookies are made with love, using only the finest ingredients. Whether you’re looking for a post-workout snack, a midday pick-me-up, or a delightful dessert, we have something for everyone! Join us on this delicious journey to a healthier lifestyle, one cookie at a time. Dive into our mouthwatering selection and discover how indulgent health can truly be!"
-
+const heading = 'Oak & Pine Bakehouse';
+const intro =
+  "At Oak & Pine Bakehouse, we believe that indulgence can be both delicious and mindful. Whether you're craving a sweet treat or celebrating a special occasion, our freshly made cookie cakes and frostings are crafted with care using premium, and often organic and locally sourced ingredients. We prioritize quality and strive to minimize artificial and harmful additives, ensuring you can enjoy a delightful dessert without compromise. Join us in savoring the joy of treats that nourish both body and soul.";
+const disclaimer = 'A Cottage Bakery in Normal, IL';
+const content = `${intro}<br /><br />${disclaimer}`;
 export const Hero = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24" data-sb-object-id={props.id}>
-      <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
-       <div className="w-full max-w-xl mx-auto flex-1">
+      <div
+        className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${
+          themeClassMap[props.theme] ?? themeClassMap['imgRight']
+        }`}
+      >
+        <div className="w-full max-w-xl mx-auto flex-1">
           <h3 className="mb-6 text-3xl font-bold sm:text-4x1" data-sb-field-path="heading">
             {heading}
           </h3>
@@ -26,11 +32,9 @@ export const Hero = (props) => {
           {/* {props.button && <Button {...props.button} />} */}
         </div>
         <div className="w-full aspect-[4/3] flex-1 relative overflow-hidden rounded-md">
-        <img src="/oakandpinelogo.png" alt="oak & pine bakehouse logo"/>
-        
+          <img src="/oakandpinelogo.png" alt="oak & pine bakehouse logo" />
         </div>
       </div>
     </div>
   );
-  
 };
