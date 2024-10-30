88291955971a6f446e0c73fdd0ff658f5733d296
Rommel <devegus@gmail.com>
2024-10-29 20:50:04 -0500

    added pricing

--- src/components/FormTest.jsx
+++ src/components/FormTest.jsx
@@ -15,6 +15,15 @@ const FormTest = () => {
   } = useForm();
   const [startDate, setStartDate] = useState(null);
   const [isOption2Checked, setIsOption2Checked] = useState(false);
+  const DELUXE_PRICE = 34.99; // Set the price for Deluxe Chocolate Chip
+  const DARK_CHOCOLATE_PRICE = 36.99; // Set the price for Dark Chocolate Cashew & Sea Salt
+  const DOUBLE_DELUXE_PRICE = 59.99; // Set the price for Deluxe Chocolate Chip
+  const DOUBLE_DARK_CHOCOLATE_PRICE = 64.99; // Set the price for Dark Chocolate Cashew & Sea Salt
+  const [deluxeQuantity, setDeluxeQuantity] = useState(0);
+  const [darkChocolateQuantity, setDarkChocolateQuantity] = useState(0);
+  const [doubleDeluxeQuantity, setDoubleDeluxeQuantity] = useState(0);
+  const [doubleDarkChocolateQuantity, setDoubleDarkChocolateQuantity] = useState(0);
+  const ILLINOIS_TAX_RATE = 0.0625; // 6.25% sales tax
 
   const isBlockedDate = (date) => {
     const day = date.getDay();
@@ -38,6 +47,29 @@ const FormTest = () => {
     } catch (error) {
       console.log('FAILED...', error);
     }
+  };
+
+  const handleDeluxeQuantityChange = (e) => {
+    const quantity = parseInt(e.target.value) || 0;
+    setDeluxeQuantity(quantity);
+    setValue('cookie-1-quantaty', quantity);
+  };
+
+  const calculateTotalPrice = () => {
+    const subtotal =
+      DELUXE_PRICE * deluxeQuantity +
+      DARK_CHOCOLATE_PRICE * darkChocolateQuantity +
+      DOUBLE_DELUXE_PRICE * doubleDeluxeQuantity +
+      DOUBLE_DARK_CHOCOLATE_PRICE * doubleDarkChocolateQuantity;
+
+    const tax = subtotal * ILLINOIS_TAX_RATE;
+    const totalWithTax = subtotal + tax;
+
+    return {
+      totalWithTax: totalWithTax.toFixed(2),
+      tax: tax.toFixed(2),
+      subTotal: subtotal.toFixed(2),
+    }; // Return total as a fixed decimal value
   };
 
   const cardStyle = {
@@ -69,15 +101,15 @@ const FormTest = () => {
         Specialty Cookie Cakes
       </h2>
 
+      {/* Deluxe Chocolate Chip Cookie */}
       <div style={cardStyle}>
         <label>
           <input type="checkbox" value="Deluxe Chocolate Chip" {...register('cookie-1')} />
           #1 <b> Deluxe Chocolate Chip</b>; milk, semi, dark chips frosted w/ vanilla and chocolate frostings.
         </label>
         <div style={{ marginTop: '10px', display: 'flex' }}>
-
           <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginLeft: '25px' }}>
-            <select {...register('cookie-1-quantaty')} style={{ marginRight: '10px' }}>
+            <select value={deluxeQuantity} onChange={handleDeluxeQuantityChange} style={{ marginRight: '10px' }}>
               <option value="">QTY</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -85,10 +117,12 @@ const FormTest = () => {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
+            <span style={{ marginLeft: '10px' }}>Price: ${DELUXE_PRICE}</span>
           </div>
         </div>
       </div>
 
+      {/* Dark Chocolate Cashew & Sea Salt Cookie */}
       <div style={cardStyle}>
         <label>
           <input
@@ -107,12 +141,15 @@ const FormTest = () => {
           #2 <b> Dark Chocolate Cashew & Sea Salt</b>; dairy-free, vanilla frosted (dairy free)
         </label>
         <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginLeft: '25px' }}>
-          <label>
-            <input type="checkbox" {...register('no-cashews-selected')} /> No Cashews
-          </label>
-
-          <div style={{ display: 'flex', alignItems: 'center', marginLeft: '25px' }}>
-            <select {...register('cookie-2-quantaty')} style={{ marginRight: '10px' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <select
+              value={darkChocolateQuantity}
+              onChange={(e) => {
+                setDarkChocolateQuantity(Number(e.target.value));
+                setValue('cookie-2-quantaty', e.target.value);
+              }}
+              style={{ marginRight: '10px' }}
+            >
               <option value="">QTY</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -120,6 +157,10 @@ const FormTest = () => {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
+            <label>
+              <input type="checkbox" {...register('no-cashews-selected')} /> No Cashews
+            </label>
+            <span style={{ marginLeft: '10px' }}>Price: ${DARK_CHOCOLATE_PRICE}</span>
           </div>
         </div>
       </div>
@@ -131,7 +172,15 @@ const FormTest = () => {
         </label>
         <div style={{ marginTop: '10px', display: 'flex' }}>
           <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginLeft: '25px' }}>
-            <select {...register('cookie-1-quantaty')} style={{ marginRight: '10px' }}>
+            <select
+              {...register('cookie-1-quantaty')}
+              style={{ marginRight: '10px' }}
+              value={doubleDeluxeQuantity}
+              onChange={(e) => {
+                setDoubleDeluxeQuantity(Number(e.target.value));
+                setValue('double-cookie-2-quantaty', e.target.value);
+              }}
+            >
               <option value="">QTY</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -139,6 +188,7 @@ const FormTest = () => {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
+            <span style={{ marginLeft: '10px' }}>Price: ${DOUBLE_DELUXE_PRICE}</span>
           </div>
         </div>
       </div>
@@ -161,12 +211,16 @@ const FormTest = () => {
           #4 <b> Dark Chocolate Cashew & Sea Salt</b>; dairy-free, vanilla frosted (dairy free)
         </label>
         <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginLeft: '25px' }}>
-          <label>
-            <input type="checkbox" {...register('no-cashews-selected')} /> No Cashews
-          </label>
-
-          <div style={{ display: 'flex', alignItems: 'center', marginLeft: '25px' }}>
-            <select {...register('cookie-2-quantaty')} style={{ marginRight: '10px' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <select
+              {...register('cookie-2-quantaty')}
+              style={{ marginRight: '10px' }}
+              value={doubleDarkChocolateQuantity}
+              onChange={(e) => {
+                setDoubleDarkChocolateQuantity(Number(e.target.value));
+                setValue('double-cookie-2-quantaty', e.target.value);
+              }}
+            >
               <option value="">QTY</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -174,6 +228,10 @@ const FormTest = () => {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
+            <label>
+              <input type="checkbox" {...register('no-cashews-selected')} /> No Cashews
+            </label>
+            <span style={{ marginLeft: '10px' }}>Price: ${DOUBLE_DARK_CHOCOLATE_PRICE}</span>
           </div>
         </div>
       </div>
@@ -191,6 +249,11 @@ const FormTest = () => {
             placeholderText="Select a date"
             required
           />
+          <div style={{ marginLeft: '20px', fontWeight: 'bold' }}>
+            <div>SubTotal: ${calculateTotalPrice().subTotal}</div>
+            <div>Tax: ${calculateTotalPrice().tax}</div>
+            <div>Total: ${calculateTotalPrice().totalWithTax}</div>
+          </div>
         </div>
         <input type="hidden" {...register('selectedDate', { required: 'Date is required' })} />
         {errors.selectedDate && <span style={{ color: 'red' }}>{errors.selectedDate.message}</span>}
@@ -200,7 +263,9 @@ const FormTest = () => {
         <sup style={{ fontWeight: 'bold', textAlign: 'center', lineHeight: 'normal' }}>
           **48 HOUR NOTICE IS REQUIRED BEFORE SCHEDULED PICKUP**
         </sup>
-        <p style={{ fontWeight: 'bold', textAlign: 'center', fontSize: '10px', marginTop: '10px' }}>Pickup in West Normal</p>
+        <p style={{ fontWeight: 'bold', textAlign: 'center', fontSize: '10px', marginTop: '10px' }}>
+          Pickup in West Normal
+        </p>
       </div>
 
       <h2 style={{ textAlign: 'center', color: '#2B7EC3', margin: '20px 0', fontSize: '20px', fontWeight: 'bold' }}>
@@ -213,24 +278,24 @@ const FormTest = () => {
       })}
       {renderInput('phone', 'Telephone', 'tel', { required: 'Phone number is required' })}
 
-      <div className="bottomMargin" style={{display: 'flex'}}>
-        <label style={{marginRight: '10px'}}>Select your contact preference(s):</label>
-        <label style={{marginRight: '10px'}}>
-        Email
+      <div className="bottomMargin" style={{ display: 'flex' }}>
+        <label style={{ marginRight: '10px' }}>Select your contact preference(s):</label>
+        <label style={{ marginRight: '10px' }}>
+          Email
           <input
-          style={{marginLeft: '10px'}}
+            style={{ marginLeft: '10px' }}
             type="radio"
             value="email"
             {...register('contactPreference', { required: 'You must select an option' })}
           />
         </label>
-        <label style={{marginRight: '10px'}}>
-        Phone
-          <input style={{marginLeft: '10px'}} type="radio" value="phone" {...register('contactPreference')} /> 
+        <label style={{ marginRight: '10px' }}>
+          Phone
+          <input style={{ marginLeft: '10px' }} type="radio" value="phone" {...register('contactPreference')} />
         </label>
-        <label style={{marginRight: '10px'}}>
-        Text
-          <input style={{marginLeft: '10px'}} type="radio" value="text" {...register('contactPreference')} /> 
+        <label style={{ marginRight: '10px' }}>
+          Text
+          <input style={{ marginLeft: '10px' }} type="radio" value="text" {...register('contactPreference')} />
         </label>
       </div>
 
